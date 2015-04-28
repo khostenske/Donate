c60d6e32cd41028c54d3f8aa4f6dae2505b50ba1
sbradley3 <sbradley3@zagmail.gonzaga.edu>
2015-04-28 00:53:09 -0700

    Update homePage.php

--- homePage.php
+++ homePage.php
@@ -1,31 +1,131 @@
-<?php
-	session_start();
-	include('database.php');
-?>
-<!DOCTYPE hmtl>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1/EN"
-"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<!DOCTYPE html>
 <html>
 <head>
-<!--
-	file name: tunaCafe.php
--->
-	<title>Tuna Cafe</title>
-	<link href="tunaCafe.css" rel="stylesheet" type="text/css" />
+	<title>Donation Nation</title>
+	<link href="style.css" rel="stylesheet" type="text/css"/>
 </head>
 <body>
-<h3>Welcome to Tuna Cafe</h3>
-<form action="product.php" method="POST">
-<table border="0">
-	<tr>
-		<td class="left">
-		<span class="fmlabel">Customer ID (C001, C002, C003, C004, or C005), please</span><br />
-		<label for="customerid">Customer ID: </label>
-		<input name="customerid" id="customerid" /></td>
-		<tr><td>
-			<input type="submit" value="Submit" name='submit'>
+	<header>
+		<hr>
+		<table>
+		<tr>
+			<td>
+				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp;
+				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+			</td>
+			<td>
+				<a href="homePage.php"><img src="logo.png.png" alt="logo" /></a>
+			</td>
+			
+			<td>
+				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp;
+				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+			</td>
+			<td>
+				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp;
+				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+			</td>
+			<td>
+				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp;
+				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+			</td>
+			<td>
+				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp;
+				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+			</td>
+			<td>
+				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp;
+				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+			</td>
+			<td>
+				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp;
+				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+			</td>
+			<td>
+				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp;
+				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+			</td>
+			<td>
+				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp;
+				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+			</td>
+			<td>
+				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp;
+				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+			</td>
+			<td>
+				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp;
+				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+			</td>
+			<td>
+			 	<a href="" align="right">Sign Up</a>
+				<!-- sign up and sign in pages are called???
+				-->	
+			</td>
+			<td>
+				<div style="border-left:medium #909090 solid; height:20px;" />
+				</div>
+			</td>
+			<td>
+				<a href="">Sign In</a>
+			</td>
+			<td>
+				<div style="border-left:medium #909090 solid; height:20px;" />
+				</div>
+			</td>
+			<td>
+				<a href="donationForm.php">Donate Now</a>
+			</td>
+			</tr>
+			
+			
+		</table>
+<hr>		
+	</header>
+	<div style="background-color:#909090; color:#909090; padding:20px;">
+	<table align="center">
+		<tr>
+			<td>
+				<a href="communityDevelopmentOrgs.php">
+				<img class="roundrect" src="communityDevelopmentOrgs.jpg" alt="communityDevelopmentOrgs" height="200" width="250" border="5" />
+				</a>
+			</td>
+			<td>
+				<a href="educationOrgs.php">
+				<img class="roundrect" src="educationOrgs.jpg" alt="educationOrgs" height="200" width="250"border="5" />
+				</a>
+			</td>
+			<td>
+				<a href="environmentalOrgs.php">
+				<img class="roundrect" src="environmentalOrgs.jpg" alt="environmentalOrgs" height="200" width="250" border="5" />
+				</a>
 			</td>
 		</tr>
-</table>
-</form>
-</body></html>
+	</table>
+	<br />
+	<table align="center">
+		<tr>
+			<td>
+				<a href="medicineOrgs.php">
+				<img class="roundrect" src="medicineOrgs.jpg" alt="medicineOrgs" height="200" width="250" border="5" />
+				</a>
+			</td>
+			<td>
+				<a href="otherOrgs.php">
+				<img class="roundrect" src="otherOrgs.jpg" alt="otherOrgs" height="200" width="250" border="5" />
+				</a>
+			</td>
+		</tr>
+	</table>
+	
+<footer>
+	<p>Contact us at email at <a href="donationnation@donation.org">donationnation@donate.org</a>
+	</p>
+	<br />
+	<a href="http://facebook.com"><img src="facebook.jpg" height="40" width="40"></a>
+	<a href="http://twitter.com"><img src="twitter.jpg" height="40" width="40"></a>
+
+</footer>
+</div>
+</body>
+</html>
