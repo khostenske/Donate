3def32e072c223982b9b8fe6ad6c9fdc05966c67
jpalmer7 <jpalmer7@zagmail.gonzaga.edu>
2015-04-28 22:43:03 -0700

    Update environmentalOrgs.php

--- environmentalOrgs.php
+++ environmentalOrgs.php
@@ -1,1 +1,84 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Donation Nation</title>
+	<link href="categories.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+	<h1>Donation Nation</h1>
+	<div align='left' class='title-wrap'><div class='row'>
+		<div class='columns large-12'>
+		<h2>Environmental</h2>
+		</div></div></div>
+	<table border='1'>
+	<col width="500">
+	<tr>
+	<td>
+	<div class='row content'> 
+		<div class='columns large-3 project'>
+			<div class='head'>
+			    <h2>GreenPeace Fund</h2>
+			</div>
+			<div class='body'>
+					<h2>Worldwide</h2>
+					<p>Since 1971, Greenpeace has dedicated its time, passion and resources to a mission that seeks to expose global environmental problems while ensuring that the Earth is able to nurture life in all its diversity and promote solutions for future generations.
+					It's a mission we will continue to pursue with tireless determination. But we cannot do it without your support.
+					</br></br>
+			</br></br></br></br><h2>Project Funding</h2>
+				<h3>GreenPeace's partners and private donators who provide Greenpeace with the funding necessary to protect the earth</h3>
+			<input type="submit" value="Donate">
+			</div></div></div>
+	</td>
+<td>	
+			
+	<div class='row content'> 
+		<div class='columns large-3 project'>
+			<div class='head'>
+			    <h2>Friends of the Earth</h2>
+			</div>
+			<div class='body'>
+					<h2>Worldwide</h2>
+					<p>Friends of the Earth is working towards ensuring that in our struggle for a healthy and just world our words and actions uphold the rights and dignity of all peoples. We believe that freedom from oppression is the foundation of justice and peace in the world and that oppression,
+					in all its forms, will continue to divide our movements and weaken our power to create the change we seek unless we openly acknowledge and address it in all its forms. </p>
+			</br></br><h2>Project Funding</h2>
+				<h3>Friends of the earth is supported by partnerships as well as donations from people around the world</h3>
+			<input type="submit" value="Donate">
+			</div></div></div>
+	</td>
+<td>
+	<div class='row content'> 
+		<div class='columns large-3 project'>
+			<div class='head'>
+			    <h2>Rainforest Alliance</h2>
+			</div>
+			<div class='body'>
+					<h2>Worldwide</h2>
+					<p>At the heart of the Rainforest Alliance's approach is the understanding that the health of the land is inextricably connected to the wellbeing of those who depend on it for their livelihoods.We envision a world where
+					people can thrive and prosper in harmony with the land.
+					Our approach includes training and certification to promote healthy ecosystems and communities in some of the world's most vulnerable ecosystems.</p>
+			</br></br><h2>Project Funding</h2>
+				<h3>Rainforest Alliance engage businesses and private donor to join our efforts to help protect vulnerable nature in our world</h3>
+			<input type="submit" value="Donate">
+			</div></div></div>
+	</td>
+<td>
+	<div class='row content'> 
+		<div class='columns large-3 project'>
+			<div class='head'>
+			    <h2>Ocean Conservency</h2>
+			</div>
+			<div class='body'>
+					<h2>Worldwide</h2>
+					<p>Ocean Conservancy works to keep the ocean healthy, to keep us healthy.
+Our goal is to bring people together to find solutions for our water planet. We educate and empower people to defend not only the ocean and its wildlife, but also the millions who earn their living from the ocean.
+					With your help, Ocean Convservancy is mobilizing citizen advocates to protect the ocean for future generations</p>
+			</br></br></br><h2>Project Funding</h2>
+				<h3>Ocean Conservancy partners with unexpected allies to develop cross-cutting solutions that lead to lasting change.</h3>
+			<input type="submit" value="Donate">
+			</div></div></div>
+		</td>
+		</tr>
+	
+	</table>
+</body>
+</html>
