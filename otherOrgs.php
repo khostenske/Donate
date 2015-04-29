1684625097fbe5281edc1c9f6b373121c58986ad
jpalmer7 <jpalmer7@zagmail.gonzaga.edu>
2015-04-28 22:44:19 -0700

    Update otherOrgs.php

--- otherOrgs.php
+++ otherOrgs.php
@@ -1,1 +1,83 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Donation Nation</title>
+	<link href="categories.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+	<h1>Donation Nation</h1>
+	<div align='left' class='title-wrap'><div class='row'>
+		<div class='columns large-12'>
+		<h2>Other Organizations</h2>
+		</div></div></div>
+	<table border='1'>
+	<col width="500">
+	<tr>
+	<td>
+	<div class='row content'> 
+		<div class='columns large-3 project'>
+			<div class='head'>
+			    <h2>The Salvation Army</h2>
+			</div>
+			<div class='body'>
+					<h2>Worldwide</h2>
+					<p>The Salvation Army, an international movement, is an evangelical part of the universal Christian 
+Church. Its message is based on the Bible. Its ministry is motivated by the love of God. Its mission 
+is to preach the gospel of Jesus Christ and to meet human needs in His name without discrimination.
+					</br></br>
+			</br></br></br></br><h2>Project Funding</h2>
+				<h3>The Salvation Army partners with many organizations and donors to help those in need when disaster strikes.</h3>
+			<input type="submit" value="Donate">
+			</div></div></div>
+	</td>
+<td>	
+			
+	<div class='row content'> 
+		<div class='columns large-3 project'>
+			<div class='head'>
+			    <h2>Feeding America</h2>
+			</div>
+			<div class='body'>
+					<h2>United States</h2>
+					<p>The Feeding America network is the nation's largest domestic hunger-relief organization. The nationwide network of food banks provides more than 3.3 billion meals
+					to virtually every community in the United States through food pantries and meal programs. </p>
+			</br></br></br></br></br><h2>Project Funding</h2>
+				<h3>Feeding America has a huge amount of parners and donors helping them on their quest to feed America.</h3>
+			<input type="submit" value="Donate">
+			</div></div></div>
+	</td>
+<td>
+	<div class='row content'> 
+		<div class='columns large-3 project'>
+			<div class='head'>
+			    <h2>Goodwill</h2>
+			</div>
+			<div class='body'>
+					<h2>United States</h2>
+					<p>Goodwill works to enhance the dignity and quality of life of individuals and families by strengthening communities, eliminating barriers to opportunity, and helping people in need reach
+					their full potential through learning and the power of work.</p>
+			</br></br></br></br></br></br><h2>Project Funding</h2>
+				<h3>Goodwill has several well known partners, who along with private donors help Goodwill towards their goals.</h3>
+			<input type="submit" value="Donate">
+			</div></div></div>
+	</td>
+<td>
+	<div class='row content'> 
+		<div class='columns large-3 project'>
+			<div class='head'>
+			    <h2>Boy's and Girl's Club</h2>
+			</div>
+			<div class='body'>
+					<h2>United States</h2>
+					<p>Our Mission: To enable all young people, especially those who need us most, to reach their full potential as productive, caring, responsible citizens.
+					Boy's and Girl's Clubs have been following this mission for more than 150 years.</p>
+			</br></br></br></br></br></br><h2>Project Funding</h2>
+				<h3>Boy's and Girl's Club parters with many organizations and donors to ensure their mission is acheived, year after year.</h3>
+			<input type="submit" value="Donate">
+			</div></div></div>
+		</td>
+		</tr>
+	
+	</table>
+</body>
+</html>
