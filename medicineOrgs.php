48f10d7edda82fdfadd0e2a1532f57734ee58f0a
jpalmer7 <jpalmer7@zagmail.gonzaga.edu>
2015-04-28 22:43:38 -0700

    Update medicineOrgs.php

--- medicineOrgs.php
+++ medicineOrgs.php
@@ -1,1 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Donation Nation</title>
+	<link href="categories.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+	<h1>Donation Nation</h1>
+	<div align='left' class='title-wrap'><div class='row'>
+		<div class='columns large-12'>
+		<h2>Medicine</h2>
+		</div></div></div>
+	<table border='1'>
+	<col width="500">
+	<tr>
+	<td>
+	<div class='row content'> 
+		<div class='columns large-3 project'>
+			<div class='head'>
+			    <h2>Aid for Africa</h2>
+			</div>
+			<div class='body'>
+					<h2>Africa</h2>
+					<p>Whether they are collecting and distributing books to school children, introducing medical strategies to combat the spread of HIV/AIDS, or identifying and supporting women's micro-enterprise projects, Aid for Africa's members have one thing in common
+					a passionate desire to help the people of Africa solve critical problems and build a better future for their children and communities.
+					</br></br>
+			</br></br></br></br><h2>Project Funding</h2>
+				<h3>Aid for Africa partners with businesses as well as members who help solve medical problems the african people suffer from.</h3>
+			<input type="submit" value="Donate">
+			</div></div></div>
+	</td>
+<td>	
+			
+	<div class='row content'> 
+		<div class='columns large-3 project'>
+			<div class='head'>
+			    <h2>Red Cross</h2>
+			</div>
+			<div class='body'>
+					<h2>United States/Worldwide</h2>
+					<p>The American Red Cross exists to provide compassionate care to those in need. Our network of generous donors, volunteers and employees share a mission
+					of preventing and relieving suffering, here at home and around the world, through five key service areas: Disaster Relief, Supporting America's Military Families, Lifesaving Blood, Health and Safety Services, and International Services</p>
+			</br></br></br></br><h2>Project Funding</h2>
+				<h3>Red Cross has many different partners and ordinary people who donate many different ways to support their efforts</h3>
+			<input type="submit" value="Donate">
+			</div></div></div>
+	</td>
+<td>
+	<div class='row content'> 
+		<div class='columns large-3 project'>
+			<div class='head'>
+			    <h2>Child Health Foundation</h2>
+			</div>
+			<div class='body'>
+					<h2>United States</h2>
+					<p>We operate as an independent agent of change, seeking and promoting more effective, easily-delivered and less-expensive methods
+					to prevent illness and death of children, such as oral rehydration therapy to prevent dehydration and immunizations.
+					We plan and support clinical research to develop new health care technologies for children affected by disease or disaster, and disseminate and implement proven, effective results through our network worldwide.
 
+</p>
+			</br></br><h2>Project Funding</h2>
+				<h3>Child Health Foundation partners with other organizations, using donations to save as many children's live as possible</h3>
+			<input type="submit" value="Donate">
+			</div></div></div>
+	</td>
+<td>
+	<div class='row content'> 
+		<div class='columns large-3 project'>
+			<div class='head'>
+			    <h2>Cancer Research Institute</h2>
+			</div>
+			<div class='body'>
+					<h2>WorldWide</h2>
+					<p>For more than 100 years, immunology and immunotherapy have played an ever-increasing role in the understanding and treatment of cancer. Since 1953, CRI and CRI scientists have played a major role in bringing about and accelerating this transformation.
+					Our timeline, spanning 1891 to today and containing more than 200 entries, shows some of the major discoveries and milestones in the field, highlighting CRI's impact along the way.</p>
+			</br></br></br><h2>Project Funding</h2>
+				<h3>Cancer Research Institute has many partners and private donators helping treat, and eventually find a cure for cancer</h3>
+			<input type="submit" value="Donate">
+			</div></div></div>
+		</td>
+		</tr>
+	
+	</table>
+</body>
+</html>
