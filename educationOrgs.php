e8b87bf45d214011ae8682fce4b34e4edf048cab
jpalmer7 <jpalmer7@zagmail.gonzaga.edu>
2015-04-28 22:42:23 -0700

    Update educationOrgs.php

--- educationOrgs.php
+++ educationOrgs.php
@@ -1,1 +1,84 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Donation Nation</title>
+	<link href="categories.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+	<h1>Donation Nation</h1>
+	<div align='left' class='title-wrap'><div class='row'>
+		<div class='columns large-12'>
+		<h2>Education</h2>
+		</div></div></div>
+	<table border='1'>
+	<col width="500">
+	<tr>
+	<td>
+	<div class='row content'> 
+		<div class='columns large-3 project'>
+			<div class='head'>
+			    <h2>Teach For America</h2>
+			</div>
+			<div class='body'>
+					<h2>United States</h2>
+					<p>Our mission is to enlist, develop, and mobilize our nation's most promising future leaders to grow and strengthen the movement for educational equity.
+					Teach for America began in 1989 as Wendy Kopps senior thesis at Princeton University.  Public schools, decades after desegregation, remained a realm of inequity, unable to make up for
+					the long-term effects of poverty, racism, and deeply rooted injustices.  If our country is going to adress this problem, more leaders would need to be grounded in the issues at the classroom level
+					</br></br>
+			<h2>Project Funding</h2>
+				<h3>Teach For America has various ways to partner with them, allowing them to shape the unverprivlidged youth of America</h3>
+			<input type="submit" value="Donate">
+			</div></div></div>
+	</td>
+<td>	
+			
+	<div class='row content'> 
+		<div class='columns large-3 project'>
+			<div class='head'>
+			    <h2>Books For Africa</h2>
+			</div>
+			<div class='body'>
+					<h2>Africa</h2>
+					<p>We collect, sort, ship, and distribute books to students of all ages in Africa. Our goal: to end the book famine in Africa.
+Books For Africa remains the largest shipper of donated text and library books to the African continent, shipping over 32 million books to 49 different countries since 1988.  Over the past 12 months we have shipped 2.6 million books, valued at $35 million to 27 African countries, with an additional 223 computers and 11 brand new law and human rights libraries.</p>
+			</br></br><h2>Project Funding</h2>
+				<h3>Books For Africa receives financial support from a wide variety of partners as well as private donators.</h3>
+			<input type="submit" value="Donate">
+			</div></div></div>
+	</td>
+<td>
+	<div class='row content'> 
+		<div class='columns large-3 project'>
+			<div class='head'>
+			    <h2>Pratham</h2>
+			</div>
+			<div class='body'>
+					<h2>India</h2>
+					<p>Pratham began by providing preschool education to  children in the slums of Mumbai. Before long, the Pratham balwadis (preschool classes) multiplied, and their successful model was adopted in other locations.
+					Over the years, the Pratham network has expanded to almost every state in India. Today, our direct programs including preschools, remedial learning programs and vocational classes reach hundreds of thousands of children every year. </p>
+			</br></br><h2>Project Funding</h2>
+				<h3>There are a number of partners who support Pratham in addition to it's private donors all over the world</h3>
+			<input type="submit" value="Donate">
+			</div></div></div>
+	</td>
+<td>
+	<div class='row content'> 
+		<div class='columns large-3 project'>
+			<div class='head'>
+			    <h2>Crayons for Classrooms</h2>
+			</div>
+			<div class='body'>
+					<h2>United States</h2>
+					<p>Dayton Crayons to Classrooms (DC2C) is a free store for teachers from under-funded K-12 schools that serve children living in poverty in Ohio's Miami Valley.
+DC2C lifts at-risk children's level and love of learning by collecting and distributing donated supplies and surplus products for use in the classroom.
+Teachers from eligible schools are invited to "shop" for what they need in our retail-style free store in the Goodwill Easter Seals Miami Valley building in Dayton, Ohio.</p>
+			</br></br><h2>Project Funding</h2>
+				<h3>Crayon's to Classrooms has a number of parnerships as well as private donors to support Dayton Classrooms</h3>
+			<input type="submit" value="Donate">
+			</div></div></div>
+		</td>
+		</tr>
+	
+	</table>
+</body>
+</html>
