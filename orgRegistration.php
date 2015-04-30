98fac3e7a74cabbb918a515eeb6ac995a6b7b9bb
khostenske <khostenske@zagmail.gonzaga.edu>
2015-04-29 17:39:14 -0700

    Update orgRegistration.php

--- orgRegistration.php
+++ orgRegistration.php
@@ -16,23 +16,22 @@
     <tr>
 	<td class="left">Organization Name:</td><td><input type="text" size="25" name="orgName" id="orgName"/></td>
     </tr>
-	<td class="left">Description:</td><td><input type="text" size="25" name="description" id="description"/></td>
+	<td class="left">Description:</td><td><input type="text" size="25" name="orgDescription" id="orgDescription"/></td>
 <!--Make radio buttons-->
         <td class="left">Category:</td><td><input type="text" size="25" name="category" id="category"/></td>
     </tr>
     <tr>
 	<td class="left">Headquarters Address:</td><td><input type="text" size="25" name="orgAddress" id="orgAddress"/></td>
         <td class="left">City:</td><td><input type="text" size="25" name="orgCity" id="orgCity"/></td>
-<!--Use drop down for state-->
         <td class="left">State:</td><td><input type="text" size="2" name="orgState" id="orgState"/></td>
         <td class="left">Zip Code:</td><td><input type="text" size="5" name="orgZip" id="orgZip"/></td>
     </tr>
-    <tr>
+<!--    <tr>
 	<td class="left">Contact First Name:</td><td><input type="text" size="25" name="orgFName" id="orgFName"/></td>
-        <td class="left">Last Name:</td><td><input type="text" size="25" name="orgLName" id="orgLName"/></td><br />
+       <td class="left">Last Name:</td><td><input type="text" size="25" name="orgLName" id="orgLName"/></td><br />
      	<td class="left">Phone Number:</td><td><input type="text" size="10" name="orgPhone" id="orgPhone"/></td><br />
 	<td class="left">Email:</td><td><input type="text" size="25" name="orgEmail" id="orgEmail"/></td><br />
-    </tr>
+    </tr> -->
 	<td><input type="submit" value="Register" name='submit'></td>
 	<td><input type="reset" value="Clear" name='reset'></td>
     </tr>
