fa9c45bd346708cd7145afd9c1d94e471e07affb
sbradley3 <sbradley3@zagmail.gonzaga.edu>
2015-04-29 18:36:21 -0700

    Update orgRegistration.php

--- orgRegistration.php
+++ orgRegistration.php
@@ -6,13 +6,16 @@
 <head>
 <!-- 	file name: orgRegistration.php -->
 	<title>Donation Nation</title>
-	<link href="php.css" rel="stylesheet" type="text/css"/>
+	<link href="style.css" rel="stylesheet" type="text/css"/>
 </head>
 <body>
-
+<?php
+	include('header2.php');
+?>
+<div class="green">
 <h2>Register Organization</h2>
 <form action="confirmOrgReg.php" method="POST">
-<table border="0">
+<table class="red"border="0">
     <tr>
 	<td class="left">Organization Name:</td><td><input type="text" size="25" name="orgName" id="orgName"/></td>
     </tr>
@@ -26,17 +29,19 @@
         <td class="left">State:</td><td><input type="text" size="2" name="orgState" id="orgState"/></td>
         <td class="left">Zip Code:</td><td><input type="text" size="5" name="orgZip" id="orgZip"/></td>
     </tr>
+	</table>
 <!--    <tr>
 	<td class="left">Contact First Name:</td><td><input type="text" size="25" name="orgFName" id="orgFName"/></td>
        <td class="left">Last Name:</td><td><input type="text" size="25" name="orgLName" id="orgLName"/></td><br />
      	<td class="left">Phone Number:</td><td><input type="text" size="10" name="orgPhone" id="orgPhone"/></td><br />
 	<td class="left">Email:</td><td><input type="text" size="25" name="orgEmail" id="orgEmail"/></td><br />
     </tr> -->
+	<table class="red">
 	<td><input type="submit" value="Register" name='submit'></td>
 	<td><input type="reset" value="Clear" name='reset'></td>
     </tr>
 </table>
 </form>
-
+</div>
 </body>
 </html>
