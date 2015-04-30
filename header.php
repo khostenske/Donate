7561185f70780c46fa2e7010bfda7d15978b67ad
sbradley3 <sbradley3@zagmail.gonzaga.edu>
2015-04-29 17:25:32 -0700

    Create header.php

---
+++ header.php
@@ -0,0 +1,37 @@
+<header>
+		<hr>
+		<table>
+		<tr>
+			<td>
+				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp;
+				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+			</td>
+			<td>
+				<a href="homePage.php"><img src="logo.png.png" alt="logo" /></a>
+			</td>
+			<?php
+				for($a=0;$a<=15;$a++) {
+					echo "<td>";
+					echo "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp";
+					echo "</td>";
+				}
+			?>
+			<td>
+			 	<a href="custRegistration.php" >Sign Up</a>
+				<!-- sign up and sign in pages are called???
+				-->	
+			</td>
+			
+			<td>
+				<a href="login.php">Sign In</a>
+			</td>
+			
+			<td>
+				<a href="cart.php">Your Cart</a>
+			</td>
+			</tr>
+			
+			
+		</table>
+<hr>		
+	</header>
