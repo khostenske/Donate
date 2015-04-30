213afa03e5df80bb6d711bc8f52baad275165729
sbradley3 <sbradley3@zagmail.gonzaga.edu>
2015-04-29 18:21:02 -0700

    Create header2.php

---
+++ header2.php
@@ -0,0 +1,30 @@
+<header>
+		<hr>
+		<table class="red">
+		<tr>
+			<td>
+				<a href="homePage.php"><img src="logo.png.png" alt="logo" /></a>
+			</td>
+		</tr>
+		</table>
+		<table class="red">
+		<tr>
+			<td>
+			 	<a href="custRegistration.php" >Sign Up</a>
+				<!-- sign up and sign in pages are called???
+				-->	
+			</td>
+			
+			<td>
+				<a href="login.php">Sign In</a>
+			</td>
+			
+			<td>
+				<a href="cart.php">Your Cart</a>
+			</td>
+		</tr>
+			
+			
+		</table>
+<hr>		
+	</header>
